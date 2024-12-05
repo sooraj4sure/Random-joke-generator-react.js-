875b34682aa759a6eb4f174ddc50a189c7d1bc74
sooraj4sure <skashyap9711@gmail.com>
2024-12-05 15:03:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Joker from './Joker'
+
+
+function App() {
+  return (
+    <div className='text-center mt-60'>
+
+    <Joker/>
+   
+    </div>
+  )
+}
+
+export default App
